3275af96489de88304e7466bd2a852bccbf06516
Daniells <weiyixiang@bigo.sg>
2018-07-09 16:48:54 +0800

    To use variable and the second note

---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var a int
+var b int = 5
+var c string = "test"
+
+func main(){
+	a = 10
+	d := 3.14159
+	a, b = b, a
+	e, f, g := 7, 5.5, "TEST"
+	fmt.Println(a, b, c, d, e, f, g)
+	fmt.Println()
+	fmt.Println("OK")
+}
+
